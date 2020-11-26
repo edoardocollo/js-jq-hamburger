528c646f25d82d7002c45e44e705296694dd1177
edoardocollo <edoardo.collo@gmail.com>
2020-11-26 17:17:27 +0100

    there is no power without specificity

--- main.js
+++ main.js
@@ -1,9 +1,22 @@
-$('.header-right a').click(
+// CLICK EVENTO APERTURA MENU
+///////////////////////////////////
+
+
+
+$('.header-right > a > i.fa-bars').click(
   function(){
     $('.hamburger-menu').show();
   }
 );
-$('.hamburger-menu a').click(
+
+
+
+// CLICK EVENTO CHIUSURA MENU
+///////////////////////////////////
+
+
+
+$('.hamburger-menu > a > i.fa-times').click(
   function(){
     $('.hamburger-menu').hide();
   }
